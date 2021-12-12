ef4ef195c3e3ca060a1dec47f73503ec2bd5d8cb
Duncan Nielsen <duncann008@gmail.com>
2021-12-12 13:36:03 -0600

    working on form, moved css file

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import { useState } from 'react';
+import './AddMovie.css';
 
 function AddMovie() {
 
@@ -37,38 +38,46 @@ function AddMovie() {
           ...movieToAdd,
           genre_id: event.target.value,
         });
-        console.log(movieToAdd);
       }
 
-    console.log('On AddMovie');
-    console.log(genres);
+    
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_GENRES' })
-    }, []); 
+      useEffect(() => {
+          dispatch({ type: 'FETCH_GENRES' })
+      }, []); 
 
-    const saveButton = () =>  {
-        dispatch({
-            type: 'ADD_MOVIE',
-            payload: movieToAdd
-        })
-        history.push('/');
-        console.log(movieToAdd);
-    }
+      const saveButton = () =>  {
+          dispatch({
+              type: 'ADD_MOVIE',
+              payload: movieToAdd
+          })
+          history.push('/');
+          console.log(movieToAdd);
+      }
 
-    const cancelButton = () =>  {
-        history.push('/');
-    }
+      const cancelButton = () =>  {
+          console.log('clicked Cancel');
+          let okay = confirm("If you cancel, you will lose all of your progress.");
+          if (okay === true)   {
+            history.push('/');
+            console.log('Cancel');
+          }
+          else {
+              return;
+          }
+      }
 
     return (
         <div>
         <h1>Add A Movie!</h1>
-        <form onSubmit={saveButton}>
-            <input onChange={handleTitleAdd} placeholder="Title"/><br />
-            <input onChange={handlePosterAdd} placeholder="Movie poster image URL"/><br />
-            <textarea onChange={handleDescriptionAdd} placeholder="Movie Description"/><br />          
+        <div className="addMovieForm">
+        <h2>Movie Info:</h2>
+          <form onSubmit={saveButton}>
+            Title: <input type="text" onChange={handleTitleAdd} placeholder="Title"/><br />
+            Poster URL: <input type="text" onChange={handlePosterAdd} placeholder="Movie poster image URL"/><br />
+            Description: <textarea type="text" onChange={handleDescriptionAdd} placeholder="Movie Description"/><br />          
             <select onChange={(event) => handleGenreAdd(event)}>
-                <option disabled value='0'>Pick A Genre</option>
+                <option>Pick A Genre</option>
                 {genres.map((genre) =>  {
                     return  (
                         <option key={genre.id} value={genre.id}>{genre.name}</option>
@@ -76,8 +85,17 @@ function AddMovie() {
                 })}
             </select><br />
         <button>Save</button>
-    </form>
-    <button onClick={cancelButton}>Cancel</button>
+      </form>
+      <button onClick={() => cancelButton(event)}>Cancel</button>
+    </div>
+    <div>
+      <h2>Preview:</h2>
+      <div className="previewDiv">
+        <p>{movieToAdd.title}</p>
+        <img src={movieToAdd.poster} className="previewImage"/>
+        <p>{movieToAdd.description}</p>
+      </div>
+    </div>
     </div>          
     );
 }
